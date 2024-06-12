976e9e40feb2fb4f5e06d1eacaa2ad1fb2de3a26
Trade-Offf <trade_offf@163.com>
2024-06-12 16:27:54 +0800

    优化网课脚本结构与功能

---
+++ 网课脚本/rename.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+const directory = "/Users/lijialin/Downloads/Webpack5入门与实战"; // 填入文件夹路径
+const oldStr = "【 微信号：itcodeba 】【海量课程 www.todo1024.com】"; // 填入需要替换的字符串
+const newStr = "";  // 填入替换后的字符串
+
+function renameFilesInDirectory(directory) {
+  fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+
+    files.forEach((file) => {
+      const filePath = path.join(directory, file);
+      fs.stat(filePath, (err, stats) => {
+        if (err) throw err;
+
+        if (stats.isDirectory()) {
+          // 如果是文件夹，递归遍历
+          renameFilesInDirectory(filePath);
+        } else if (stats.isFile()) {
+          // 如果是文件，检查是否需要重命名
+          if (file.includes(oldStr)) {
+            const newFilename = file.replace(oldStr, newStr);
+            fs.rename(filePath, path.join(directory, newFilename), (err) => {
+              if (err) throw err;
+              console.log(`Renamed file ${file} to ${newFilename}`);
+            });
+          }
+        }
+      });
+    });
+  });
+}
+
+renameFilesInDirectory(directory);
